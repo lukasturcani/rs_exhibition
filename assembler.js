592fd53dc26355a37b5872ab072682b8c05809f8
Lukas Turcani <lukasturcani93@gmail.com>
2017-05-01 22:52:03 +0100

    got the layout down

--- assembler.js
+++ assembler.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
-    // Render molecules.
-    var glmol02 = new GLmol('main');
+    // Render molecules and topologies.
+    var main = new GLmol('main');
+    var t1 = new GLmol('t1');
+    var t2 = new GLmol('t2');
+    var t3 = new GLmol('t3');
+    var t4 = new GLmol('t4');
+    var t5 = new GLmol('t5');
+    var t6 = new GLmol('t6');
 
 });
