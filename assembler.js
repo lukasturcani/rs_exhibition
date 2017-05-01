b1d7c8070fe1097299ecb3bfc1669a97ee1a6b50
Lukas Turcani <lukasturcani93@gmail.com>
2017-05-01 23:10:51 +0100

    more progress on laoyout

--- assembler.js
+++ assembler.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
     // Render molecules and topologies.
-    var main = new GLmol('main');
-    var t1 = new GLmol('t1');
-    var t2 = new GLmol('t2');
-    var t3 = new GLmol('t3');
-    var t4 = new GLmol('t4');
-    var t5 = new GLmol('t5');
-    var t6 = new GLmol('t6');
+    // var main = new GLmol('main');
+    // var t1 = new GLmol('t1');
+    // var t2 = new GLmol('t2');
+    // var t3 = new GLmol('t3');
+    // var t4 = new GLmol('t4');
+    // var t5 = new GLmol('t5');
+    // var t6 = new GLmol('t6');
 
 });
