56c76ed67f62c77e32dc634f7b8727822599cad4
Lukas Turcani <lukasturcani93@gmail.com>
2017-05-02 12:14:55 +0100

    stuff

--- make_cages.py
+++ make_cages.py
@@ -1,23 +1,36 @@
-from mmeam import StructUnit2, StructUnit3, FourPlusSix, EightPlusTwelve, Dodecahedron, macromodel_opt
+from mmeam import StructUnit2, StructUnit3, FourPlusSix, EightPlusTwelve, Dodecahedron, macromodel_opt, Cage, Population, FunctionData
 from glob import glob
 import json, os
+import multiprocessing as mp
+from functools import partial
 
 # %%
 
 unopt_cages = {}
 opt_cages = {}
+c = []
 for tname, t in zip(['t1', 't2', 't3'], [FourPlusSix, EightPlusTwelve, Dodecahedron]):
     for lkpath in glob('mols/lks/*'):
         for bbpath in glob('mols/bbs/*'):
-            lk = StructUnit2(lkpath)
+            lk = StructUnit2(lkpath, 'amine')
             lk_name = 'lk' + os.path.basename(lkpath).replace('.mol', '')
-            bb = StructUnit3(bbpath)
+            bb = StructUnit3(bbpath, 'aldehyde')
             bb_name = 'bb' + os.path.basename(bbpath).replace('.mol', '')
-            cage = Cage([lk, bb], t())
-            cage_name = tname + lk_name + bb_name
-            unopt_cages[cage_name] = cage.mdl_mol_block()
-            macromodel_opt(cage, '/opt/schrodinger2016-4', {'md' : True})
-            opt_cages[cage_name] = cage.mdl_mol_block()
-            
-json.dumps(unopt_cages)
-json.dumps(opt_cages)
+            cage = Cage([lk, bb], t(), tname + lk_name + bb_name)
+            unopt_cages[cage.name] = cage.mdl_mol_block()
+            c.append(cage)
+
+
+# %%
+
+# unopt_cages
+# json.dumps(unopt_cages)
+
+# %%
+
+# pop = Population(unopt_cages.values())
+# pop.ga_tools.optimization = FunctionData('macromodel_opt', macromodel_path='/opt/schrodinger2016-4', settings={'md' : True})
+# pop.optimize_population()
+# for m in pop:
+#     opt_cages[m.name] = m.mdl_mol_block()
+# json.dumps(opt_cages)
