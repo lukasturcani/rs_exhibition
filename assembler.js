85a74cebec14040f6926474fbb2ece0d007ca730
Lukas Turcani <lukasturcani93@gmail.com>
2017-05-07 20:18:08 +0100

    interaction done, probably

--- assembler.js
+++ assembler.js
@@ -1,70 +1,50 @@
 // The variables unopt_cages and opt_cages are defined in the file
 // "cagesfile".
 
-function activateNext(){
-    $("#next_button1").on("click touchstart", function() {
-        $("body").load("assembler2.html");
-    });
-    $("#next_button2").on("click touchstart", function() {
-        $("body").load("assembler3.html");
-    });
-    $("#next_button3").on("click touchstart", function() {
-        $("body").load("assembler4.html");
-    });
+function wrapNext(data) {
+    var prev_data = window.location.href.split('#');
+    if (prev_data.length == 1) {prev_data = "";}
+    else {prev_data = prev_data[prev_data.length-1];}
+    var n = window.location.href.split("assembler");
+    n = parseInt(n[n.length-1].split(".html")[0]) + 1;
+    if (n == 7) {n = 1;}
+    $('.next_button').wrap('<a href="assembler'+ n + '.html#' + prev_data +  data + '"></a>');
+}
+
+function wrapBack() {
+    if (!window.location.href.includes('#')) {return;};
+    var prev_data = window.location.href.split('#')[1];
+    if (!window.location.href.includes('assembler5')){
+        prev_data = prev_data.slice(0, prev_data.length-1);
+    }
+    var n = window.location.href.split("assembler");
+    n = parseInt(n[n.length-1].split(".html")[0]) - 1;
+    $('.back_button').wrap('<a href="assembler'+ n + '.html#' + prev_data + '"></a>');
+
 }
 
 
 $(document).ready(function() {
+    wrapBack();
 
-    // Render molecules and topologies.
-
-    $("#next_button4").on("click touchstart", function() {
-        $("body").load("assembler5.html");
-    });
-    $("#no_button").on("click touchstart", function() {
-        selected.prediction = "no";
-        $("body").load("assembler6.html");
-    });
-    $("#yes_button").on("click touchstart", function() {
-        selected.prediction = "yes";
-        $("body").load("assembler6.html");
-    });
-    $("#next_button5").on("click touchstart", function() {
-        $("body").load("assembler1.html");
-    });
-
-    $("#back_button1").on("click touchstart", function() {
-        $("body").load("assembler1.html");
-    });
-    $("#back_button2").on("click touchstart", function() {
-        $("body").load("assembler2.html");
-    });
-    $("#back_button3").on("click touchstart", function() {
-        $("body").load("assembler3.html");
-    });
-    $("#back_button4").on("click touchstart", function() {
-        $("body").load("assembler4.html");
-    });
-
-
-    $("#pore_button").on("click touchstart", function() {
+    $(document).on("click touchstart", "#pore_button", function() {
         $(this).toggleClass('selectedToggle');
 
     });
-    $("#vdw_button").on("click touchstart", function() {
+    $(document).on("click touchstart", "#vdw_button", function() {
         $(this).toggleClass("selectedToggle");
 
     });
 
 
-    $('.selectable').on("click touchstart", function() {
+    $(document).on("click touchstart", ".selectable", function() {
         $('.selected').removeClass('selected');
         $(this).addClass('selected');
-        c = Array(this.classList).filter(function(x) { return (x=='bb' || x=='lk' || x=='top'); })[0];
-        selected[c] = this.id;
+        c = [for (x of this.classList) if (x=='bb' || x=='lk' || x=='top') x][0];
 
         $('.next_button').addClass('ready');
-        activateNext();
+        wrapNext(this.id);
+
     });
 
 
