6db414600dc5e51f134361fb93742ee421150378
Lukas Turcani <lukasturcani93@gmail.com>
2017-05-07 11:30:27 +0100

    added interaction

--- assembler.js
+++ assembler.js
@@ -31,50 +31,89 @@ $(document).ready(function() {
 
     // Render molecules and topologies.
 
-
-    var lk1 = new GLmol('lk1');
-    var lk2 = new GLmol('lk2');
-    var lk3 = new GLmol('lk3');
-    var lk4 = new GLmol('lk4');
-    var lk5 = new GLmol('lk5');
-    var lk6 = new GLmol('lk6');
-
-    var bb1 = new GLmol('bb1');
-    var bb2 = new GLmol('bb2');
-    var bb3 = new GLmol('bb3');
-    var bb4 = new GLmol('bb4');
-    var bb5 = new GLmol('bb5');
-    var bb6 = new GLmol('bb6');
-
-    var t1 = new GLmol('t1');
-    var t2 = new GLmol('t2');
-    var t3 = new GLmol('t3');
-
     var selected = {lk : $('#lk1'),
                     bb : $('#bb1'),
                     top: $('#t1'),
                     unopt : "t1lk1bb1"}
 
-    $('.selectable').on("click touchstart", function() {
-        // Clicking on a selectable element, first locates the its
-        // class. The highlighted element of the that class then has
-        // the highlight removed. The clicked on element then gets
-        // highlighted. Finally the cage displayed in the main window
-        // is reloaded.
-
-        removeHighlight(selected, $(this).hasClass('lk'), $(this).hasClass('bb'), $(this).hasClass('top'));
-        $(this).addClass('selected');
-        addHighlight(selected, $(this), $(this).hasClass('lk'), $(this).hasClass('bb'), $(this).hasClass('top'));
-        var cage_name = selected.top[0].id + selected.lk[0].id + selected.bb[0].id;
-        main.loadMoleculeStr(false, unopt_cages[cage_name]);
-        selected.unopt = cage_name;
+    $("#next_button1").on("click touchstart", function() {
+        $("body").load("assembler2.html");
+    });
+    $("#next_button2").on("click touchstart", function() {
+        $("body").load("assembler3.html");
+    });
+    $("#next_button3").on("click touchstart", function() {
+        $("body").load("assembler4.html");
+    });
+    $("#next_button4").on("click touchstart", function() {
+        $("body").load("assembler5.html");
+    });
+    $("#no_button, #yes_button").on("click touchstart", function() {
+        $("body").load("assembler6.html");
+    });
+    $("#next_button5").on("click touchstart", function() {
+        $("body").load("assembler1.html");
     });
 
-    $('button').on("click tap", function() {
-        main.loadMoleculeStr(false, opt_cages[selected.unopt]);
+
+    $("#back_button1").on("click touchstart", function() {
+        $("body").load("assembler1.html");
+    });
+    $("#back_button2").on("click touchstart", function() {
+        $("body").load("assembler2.html");
+    });
+    $("#back_button3").on("click touchstart", function() {
+        $("body").load("assembler3.html");
+    });
+    $("#back_button4").on("click touchstart", function() {
+        $("body").load("assembler4.html");
     });
 
-    var main = new GLmol('main');
+
+    $("#pore_button").on("click touchstart", function() {
+
+        if ($(this).hasClass('selectedToggle')) {
+            $(this).fadeTo('fast', 0.5)
+        }
+        else {
+            $(this).fadeTo('fast', 1)
+        }
+
+        $(this).toggleClass('selectedToggle');
+
+    });
+    $("#vdw_button").on("click touchstart", function() {
+
+        if ($(this).hasClass('selectedToggle')) {
+            $(this).fadeTo('fast', 0.5)
+        }
+        else {
+            $(this).fadeTo('fast', 1)
+        }
+
+        $(this).toggleClass('selectedToggle');
+
+    });
+
+
+    // $('.selectable').on("click touchstart", function() {
+    //     // Clicking on a selectable element, first locates the its
+    //     // class. The highlighted element of the that class then has
+    //     // the highlight removed. The clicked on element then gets
+    //     // highlighted. Finally the cage displayed in the main window
+    //     // is reloaded.
+    //
+    //     removeHighlight(selected, $(this).hasClass('lk'), $(this).hasClass('bb'), $(this).hasClass('top'));
+    //     $(this).addClass('selected');
+    //     addHighlight(selected, $(this), $(this).hasClass('lk'), $(this).hasClass('bb'), $(this).hasClass('top'));
+    //     var cage_name = selected.top[0].id + selected.lk[0].id + selected.bb[0].id;
+    //     main.loadMoleculeStr(false, unopt_cages[cage_name]);
+    //     selected.unopt = cage_name;
+    // });
+    //
+    // $('button').on("click tap", function() {
+    //     main.loadMoleculeStr(false, opt_cages[selected.unopt]);
+    // });
 
 
 });
