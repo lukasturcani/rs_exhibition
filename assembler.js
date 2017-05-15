b505191532f48087a46111b7f66d9fc27d91fa0d
Lukas Turcani <lukasturcani93@gmail.com>
2017-05-15 17:43:18 +0100

    added topologies

--- assembler.js
+++ assembler.js
@@ -28,8 +28,8 @@ $(document).ready(function() {
     var t1 = new GLmol('1');
     var t2 = new GLmol('2');
     var t3 = new GLmol('3');
-    // var t4 = new GLmol('4');
-    // var t5 = new GLmol('5');
+    var t4 = new GLmol('4');
+    var t5 = new GLmol('5');
 
     wrapBack();
 
