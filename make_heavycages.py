30430d3865f6ec12a8db971d59830ce4c3d840f4
Lukas Turcani <lukasturcani93@gmail.com>
2017-06-12 15:25:43 +0100

    heavy cages added and working

---
+++ make_heavycages.py
@@ -0,0 +1,24 @@
+from mmeam import (Population, StructUnit2, StructUnit3,
+                   Cage, TwoPlusThree, FourPlusSix, EightPlusTwelve,
+                   Dodecahedron)
+from glob import glob
+import os
+import json
+
+# Load all unoptimized cages and rebuild them using correct names.
+unopt_cages = {}
+for topi, top in enumerate([TwoPlusThree, FourPlusSix,
+                            EightPlusTwelve, Dodecahedron], 1):
+    for bb_file in glob('mols/bbs/*.mol'):
+        for lk_file in glob('mols/lks/*.mol'):
+            bb = StructUnit3(bb_file, 'aldehyde')
+            lk = StructUnit2(lk_file, 'amine')
+            c = Cage([bb, lk], top())
+            bbname = os.path.basename(bb_file).replace('.mol', '')
+            lkname = os.path.basename(lk_file).replace('.mol', '')
+            c.name = str(topi) + bbname + lkname
+            print(c.name)
+            unopt_cages[c.name] = c.mdl_mol_block()
+
+with open('heavycages', 'w') as f:
+    json.dump(unopt_cages, f)
