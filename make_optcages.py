b36454e45ae45a399e5e7fe0414b7f2560bce673
Lukas Turcani <lukasturcani93@gmail.com>
2017-06-12 22:09:10 +0100

    need to change carbon back to gray

--- make_optcages.py
+++ make_optcages.py
@@ -6,25 +6,42 @@ import os
 import json
 
 # Load all unoptimized cages and rebuild them using correct names.
-opt_cages = {}
+
 opt_pop = Population.load('opt_cages.json')
-not_in_pop = []
+new_opt_pop = Population()
+to_be_opt = Population()
+opt_cage_dict = {}
 for topi, top in enumerate([TwoPlusThree, FourPlusSix,
                             EightPlusTwelve, Dodecahedron], 1):
     for bb_file in glob('mols/heavy/bbs/*.mol'):
         for lk_file in glob('mols/heavy/lks/*.mol'):
-            bb = StructUnit3(bb_file, 'aldehyde')
-            lk = StructUnit2(lk_file, 'amine')
-            c = Cage([bb, lk], top())
+            # build the heavy cage.
+            bbh = StructUnit3(bb_file, 'aldehyde')
+            lkh = StructUnit2(lk_file, 'amine')
+            ch = Cage([bbh, lkh], top())
 
-            bbname = os.path.basename(bb_file).replace('.mol', '')
-            lkname = os.path.basename(lk_file).replace('.mol', '')
-            c.name = str(topi) + bbname + lkname
-            # print(c.name)
-            if c not in opt_pop:
-                print(c.name)
-                not_in_pop.append(c)
-            opt_cages[c.name] = c.mdl_mol_block()
+            # build the light cage.
+            bbl = StructUnit3(bb_file.replace('heavy', 'light'), 'aldehyde')
+            lkl = StructUnit2(lk_file.replace('heavy', 'light'), 'amine')
+            cl = Cage([bbl, lkl], top())
 
-# with open('optcages', 'w') as f:
-#     json.dump(opt_cages, f)
+            # if heavy_cage was previously optimzed update the light
+            # structure.
+            if ch in opt_pop:
+                cl.mol = ch.mol
+            else:
+                to_be_opt.members.append(cl)
+
+            new_opt_pop.members.append(cl)
+
+            name = (str(topi) +
+                    os.path.basename(bb_file).replace('.mol', '') +
+                    os.path.basename(lk_file).replace('.mol', ''))
+            opt_cage_dict[name] = cl.mdl_mol_block()
+
+
+to_be_opt.dump('needs_opt.json')
+new_opt_pop.dump('opt_cages_lukas.json')
+
+with open('optcages', 'w') as f:
+    json.dump(opt_cage_dict, f)
